9033dbf846c3008b2dcaba4dbd65a19e16f29e0f
alex <lesha1223lesha@gmail.com>
2026-06-04 15:18:12 +0300

    improve yazi openers (add imv & mpv support)

--- modules/features/user/cli/yazi/default.nix
+++ modules/features/user/cli/yazi/default.nix
@@ -35,6 +35,18 @@
               linemode = "size";
             };
             opener = {
+              copy = [
+                { run = ''wl-copy < $@''; orphan = true; desc = "Copy"; }
+              ];
+              copy_image = [
+                { run = ''wl-copy -t image/png < $@''; orphan = true; desc = "Copy"; }
+              ];
+              open_image = [
+                { run = ''imv "$@"''; orphan = true; desc = "Imv"; }
+              ];
+              open_video = [
+                { run = ''mpv "$@"''; orphan = true; desc = "Mpv"; }
+              ];
               edit_text = [
                 { run = ''nano "$@"''; block = true; desc = "Nano"; }
                 { run = ''vim "$@"''; block = true; desc = "Vim"; }
@@ -43,6 +55,8 @@
               ];
               view_pdf = [
                 { run = ''atril "$@"''; orphan = true; desc = "Atril"; }
+              ]
+              browser = [
                 { run = ''librewolf "$@"''; orphan = true; desc = "Librewolf"; }
                 { run = ''firefox "$@"''; orphan = true; desc = "Firefox"; }
               ];
@@ -74,10 +88,10 @@
                   
                 { mime = "inode/empty"; use = [ "edit_text" "reveal" "show_exif" ]; }
                   
-                { mime = "text/*"; use = [ "edit_text" "reveal" "show_exif" ]; }
+                { mime = "text/*"; use = [ "edit_text" "copy" "reveal" "show_exif" ]; }
                 { mime = "application/json"; use = [ "edit_text" "open_term" "show_exif" ]; }
                   
-                { mime = "application/pdf"; use = [ "view_pdf" "system" "reveal" "show_exif" ]; }
+                { mime = "application/pdf"; use = [ "view_pdf" "browser" "system" "reveal" "show_exif" ]; }
                   
                 { mime = "application/msword"; use = [ "edit_document" "system" "reveal" "show_exif" ]; }
                 { mime = "application/openxmlformats-officedocument.wordprocessingml.document"; use = [ "edit_document" "system" "reveal" "show_exif" ]; }
@@ -86,9 +100,9 @@
                 { mime = "application/rtf"; use = [ "edit_document" "system" "reveal" "show_exif" ]; }
                 { mime = "text/rtf"; use = [ "edit_document" "system" "reveal" "show_exif" ]; }
                   
-                { mime = "image/*"; use = [ "system" "reveal" "show_exif" ]; }
-                { mime = "video/*"; use = [ "system" "reveal" "show_exif" ]; }
-                { mime = "audio/*"; use = [ "system" "reveal" "show_exif" ]; }                  
+                { mime = "image/*"; use = [ "system" "open_image" "browser" "copy_image" "reveal" "show_exif" ]; }
+                { mime = "video/*"; use = [ "system" "open_video" "browser" "reveal" "show_exif" ]; }
+                { mime = "audio/*"; use = [ "system" "browser" "reveal" "show_exif" ]; }                  
                   
                 { url = "*"; use =[ "system" "reveal" "show_exif" ]; }
               ];
